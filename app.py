f3a30f42ecf4fd5bd4b66080557932c424144693
mallikarjuna pokuri <mallikarjunapokuri595@gmail.com>
2023-12-05 19:45:00 -0600

    Updated CICD Pipeline

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict2(tweet:str):
     return {"result":result}
 '''
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
